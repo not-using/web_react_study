4fd8fbe71d82c9fd5bc77abe169be5cd57697c10
sujikim <sujikim@student.42seoul.kr>
2022-05-30 16:03:36 +0900

    ENV: js->tsx 수정 및 사용하지 않는 기본파일 삭제

---
+++ 00_signup/src/component/main/main.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Main: React.FC = () => {
+	return <div>Main</div>;
+};
+
+export default Main;
--- 00_signup/src/index.tsx
+++ 00_signup/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Main from './component/main/main';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
