84c2e3d6291dc4cc25a5237bdc8117fbca357e71
sujikim <sujikim@student.42seoul.kr>
2022-06-07 07:08:27 +0900

    CHORE: indent 및 따옴표 수정

--- 00_signup/src/type/userInfo.ts
+++ 00_signup/src/type/userInfo.ts
@@ -1,11 +1,11 @@
 
 export type user = {
-    email: string;
-    password: string;
-    pwCheck: boolean;
-    userName: string;
-    phone: string;
-    recommender: string;
-    agreement1: boolean;
-    agreement2: boolean;
-  }
+  email: string;
+  password: string;
+  pwCheck: boolean;
+  userName: string;
+  phone: string;
+  recommender: string;
+  agreement1: boolean;
+  agreement2: boolean;
+}
