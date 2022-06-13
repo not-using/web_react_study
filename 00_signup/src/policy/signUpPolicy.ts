b427d36ba774de6347d301d3e5c1bc940cd94969
sujikim <sujikim@student.42seoul.kr>
2022-06-14 00:59:26 +0900

    FIX: 정규표현식 수정

--- 00_signup/src/policy/signUpPolicy.ts
+++ 00_signup/src/policy/signUpPolicy.ts
@@ -1,9 +1,9 @@
 import { user } from '../type/userInfo'
 
-const emailReg = RegExp(/^[0-9a-zA-Z]\w*@[0-9a-z]\w*\.[a-z]{2,3}$/);
-const passwordReg = RegExp(/[\w!@#$%^&*.]{8,42}/);
-const phoneReg = RegExp(/\d{2,3}[- ]?\d{3,4}[- ]?\d{4}/);
-const userNameReg = RegExp(/\w{2,10}/)
+const emailReg = RegExp(/(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/);
+const passwordReg = RegExp(/(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,42}/);
+const phoneReg = RegExp(/(010[0-9]{7,8})/);
+const userNameReg = RegExp(/[a-z|A-Z]{2,10}/)
 
 export const emailValidator = (email: string) => {
   return emailReg.test(email);
